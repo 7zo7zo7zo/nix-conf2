dfec515965e3f3bc601aee5aa1a34e4786fbe01a
7zo7zo7zo <77756772+7zo7zo7zo@users.noreply.github.com>
2026-05-31 14:03:36 -0700

    Configure NixOS for Lenovo with modules and hostname

--- modules/hosts/lenovo/configuration.nix
+++ modules/hosts/lenovo/configuration.nix
@@ -1,7 +1,19 @@
 { inputs, ... }:
 
 {
-  flake.nixosConfigurations.lenovo = inputs.nixpkgs.lib.nixosSystem {
-    
-  }
+	flake.nixosConfigurations.lenovo = inputs.nixpkgs.lib.nixosSystem {
+		modules = with inputs.self.modules.nixos; [
+			base
+			audio
+			bluetooth
+			printer
+
+			{
+				imports = [
+          ../hardware-configuration.nix
+        ];
+				networking.hostName = "lenovo";
+			}
+		];
+	}
 }
