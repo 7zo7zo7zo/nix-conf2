3f7151915e60ccc788f3ef7ff18a3442682aab76
7zo7zo7zo <77756772+7zo7zo7zo@users.noreply.github.com>
2026-05-31 17:24:03 -0700

    Add user configuration for Zsh and user 'steve'

---
+++ modules/user.nix
@@ -0,0 +1,24 @@
+{ inputs, ... }:
+{
+	flake.modules.nixos.user = {
+		home-manager.sharedModules = [
+			inputs.self.modules.homeManager.user
+		];
+
+		programs.zsh.enable = true;
+
+		users.users.steve = {
+			isNormalUser = true;
+			home = "/home/steve";
+			shell = pkgs.zsh;
+			extraGroups = [ "wheel" "audio" "video" ];
+		};
+	}
+
+	flake.modules.homeManager.user = {
+		user.${user} = {
+			home.username = ${user};
+			home.homeDirectory = "/home/${user}";
+		}
+	};
+}
